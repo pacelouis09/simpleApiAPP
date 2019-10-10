b0570a11af1268a7c7bee5058c04cb79d5573b64
Pace Louis <paceloui09@gmail.com>
2019-10-10 14:56:36 +0200

    deploy

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -15,7 +15,7 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'password', 'phone')
 
 class BackgroundAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'phone', 'street_address', 'address2', 'city', 'state', 'zip_code')
 
 # class OrganizationAdmin(admin.ModelAdmin):
 #     pass
